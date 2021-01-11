06763b31196c044f5beec6c75c645f67717c85b7
andrew.rosario <andrew.arosario@gmail.com>
2021-01-11 11:03:04 -0300

    enable socket communication

--- client/index.js
+++ client/index.js
@@ -1,17 +1,52 @@
 (function () {
-    "use strict";
-  
-    document.addEventListener('click', async event => {
-      if (event.target.id === 'button') {
-        try {
-            const stream = await window.navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-            const video = document.getElementById('video');
-            video.srcObject = stream;
-            video.play();
-        } catch(err) {
-            alert('Access Denied');
-        }
-      }
-    });
-  
+  "use strict";
+
+  let connection;
+
+  const enableAndDisableButtons = (connected) => {
+    document.getElementById('start').disabled = connected;
+    document.getElementById('say-hello').disabled = !connected;
+    document.getElementById('close').disabled = !connected;
+  }
+
+  const setupWebSocketConnection = () => {
+    connection = new WebSocket('ws://127.0.0.1:1337');
+
+    connection.onopen = () => {
+      addMessageToConsole('You are now connected!');
+      enableAndDisableButtons(true);
+    };
+
+    connection.onerror = error => {
+      console.log(`An error occured: ${error}`)
+    };
+
+    connection.onmessage = message => {
+      const data = JSON.parse(message.data);
+      addMessageToConsole(`Client${data.client} says: ${data.text}`)
+    };
+  }
+
+  const closeConnection = () => {
+    connection.close();
+    addMessageToConsole('You disconnected!');
+    enableAndDisableButtons(false);
+  }
+
+  const addMessageToConsole = message => {
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = message;
+    document.getElementById('console').appendChild(messageDiv);
+  }
+
+
+  document.addEventListener('click', async event => {
+    if (event.target.id === 'start') {
+      setupWebSocketConnection();
+    } else if (event.target.id === 'say-hello') {
+      connection.send('Hello!');
+    } else if (event.target.id === 'close') {
+      closeConnection();
+    }
+  });
 })();
